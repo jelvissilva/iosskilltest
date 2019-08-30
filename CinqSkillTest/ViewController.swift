809406979ab196e63306f5ac437b605b16601adf
Flavio Telles <f.paschoal@cinq.com.br>
2019-08-30 15:55:43 -0300

    Initial Commit

---
+++ CinqSkillTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CinqSkillTest
+//
+//  Created by Flavio Telles Paschoal Santos on 30/08/19.
+//  Copyright © 2019 POCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
